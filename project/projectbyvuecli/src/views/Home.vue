1f45d52643fee9a66aef30e4462627aa42259b2d
Li Kourin <1411299332@qq.com>
2019-06-16 00:15:15 +0800

    更新Vue项目

---
+++ project/projectbyvuecli/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <Index msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Index from '@/components/Index.vue'
+
+export default {
+  name: 'home',
+  components: {
+    Index
+  }
+}
+</script>
