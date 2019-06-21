232b2370f31966ad40c4a8376caf7f3131343479
keluotao <3059983129@qq.com>
2019-06-21 21:38:34 +0800

    剩下的页面全部完成

---
+++ project/project-vuecli/src/views/blogItem.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <div class="body-inner">
+
+            <header/>
+
+            <div id="banner-area">
+                <img src="images/banner/banner2.jpg" alt ="" />
+                <div class="parallax-overlay"></div>
+
+                <subpagetitle/>
+
+            </div><!-- Banner area end -->
+
+            <BlogDetailsPage/>
+
+            <div class="gap-40"></div>
+
+            <Footer1/>
+
+            <Footer2/>
+        </div><!-- Body inner end -->
+    </div>
+</template>
+
+<script>
+    import Header from "@/components/Header";
+    import Subpagetitle from "@/components/Subpagetitle";
+    import BlogDetailsPage from "@/components/BlogDetailsPage";
+    import Footer1 from "@/components/Footer1";
+    import Footer2 from "@/components/Footer2";
+    export default {
+        name: "biog-item",
+        components: {
+            Footer2,
+            Footer1,
+            BlogDetailsPage,
+            Subpagetitle,
+            Header
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ project/project-vuecli/src/views/career.vue
@@ -0,0 +1,54 @@
+<template>
+    <div>
+        <div class="body-inner">
+
+            <Header/>
+
+            <div id="banner-area">
+                <img src="images/banner/banner2.jpg" alt ="" />
+                <div class="parallax-overlay"></div>
+                <Subpagetitle/>
+            </div><!-- Banner area end -->
+
+            <Maincontainer/>
+
+            <section class="call-to-action">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <h3>Join with BizCraft and Give Your Website a Brand New Look</h3>
+                            <a href="#" class="pull-right btn btn-primary white">Purchase Now</a>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <Footer1/>
+
+            <Footer2/>
+
+        </div><!-- Body inner end -->
+    </div>
+</template>
+
+<script>
+    import Header from "@/components/Header";
+    import Subpagetitle from "@/components/Subpagetitle";
+    import Maincontainer from "@/components/Maincontainer";
+    import Footer1 from "@/components/Footer1";
+    import Footer2 from "@/components/Footer2";
+    export default {
+        name: "career",
+        components: {
+            Footer2,
+            Footer1,
+            Maincontainer,
+            Subpagetitle,
+            Header
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
