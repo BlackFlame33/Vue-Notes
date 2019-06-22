e735c8c98c393cb275aa56d5d652d39c5a947b25
Li Kourin <1411299332@qq.com>
2019-06-22 19:46:46 +0800

    ‘搬运’项目完成。接下来开始考虑加入vue指令

--- project/project-vuecli/public/js/ini.isotope.js
+++ project/project-vuecli/public/js/ini.isotope.js
@@ -1,59 +1,59 @@
-jQuery(function($) {
+jQuery(function($){
 
-    //region Isotope
-    if ($.fn.isotope) {
+	//region Isotope
+	if($.fn.isotope) {
 
-        var $w = $(window);
+		var $w = $(window);
 
-        $w.load(function() {
-            var $isotops = $('.isotope');
+		$w.load(function () {
+			var $isotops = $('.isotope');
 
-            $isotops.each(function() {
-                var $el = $(this),
-                    id = $el.attr('id'),
-                    mode = $el.data('isotopeMode') || 'fitRows',
-                    tmt;
-
-
-                $el.isotope({
-                    itemSelector: '.isotope-item',
-                    layoutMode: mode,
-                    animationOptions: {
-                        duration: 400,
-                        queue: false
-                    }
-                });
+			$isotops.each(function () {
+				var $el = $(this),
+					id = $el.attr('id'),
+					mode = $el.data('isotopeMode') || 'fitRows',
+					tmt;
 
 
-                $w.resize(function() {
-                    clearTimeout(tmt);
-                    tmt = setTimeout(function() {
-                        $el.isotope('layout');
-                    }, 150);
-                });
+				$el.isotope({
+					itemSelector: '.isotope-item',
+					layoutMode: mode,
+					animationOptions: {
+						duration: 400,
+						queue: false
+					}
+				});
 
-                var $menu = $('[data-isotope-nav="' + id + '"]');
 
-                if ($menu.length) {
-                    $menu.find('a').click(function(e) {
-                        var $link = $(this);
-                        if (!$link.hasClass('active')) {
-                            var selector = $link.attr('data-filter');
-                            $link.parents('ul').eq(0).find('.active').removeClass('active');
-                            $link.addClass('active');
-                            $el.isotope({ filter: selector });
-                        }
-                        e.preventDefault();
-                    });
-                }
+				$w.resize(function(){
+					clearTimeout(tmt);
+					tmt = setTimeout(function(){
+						$el.isotope('layout');
+					}, 150);
+				});
 
-                $w.resize();
+				var $menu = $('[data-isotope-nav="' + id + '"]');
 
-            });
+				if ($menu.length) {
+					$menu.find('a').click(function (e) {
+						var $link = $(this);
+						if(!$link.hasClass('active')){
+							var selector = $link.attr('data-filter');
+							$link.parents('ul').eq(0).find('.active').removeClass('active');
+							$link.addClass('active');
+							$el.isotope({ filter: selector });
+						}
+						e.preventDefault();
+					});
+				}
 
-        });
+				$w.resize();
 
-    }
-    //endregion
+			});
+
+		});
+
+	}
+	//endregion
 
 });
