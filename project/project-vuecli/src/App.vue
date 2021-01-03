067a4a0004b610767d4bb05e147145d5a0d96998
BlackFlame33 <blackflame33@icloud.com>
2021-01-03 22:26:14 +0800

    style(src): white-space
    格式化代码

--- project/project-vuecli/src/App.vue
+++ project/project-vuecli/src/App.vue
@@ -1,27 +1,27 @@
 <template>
-    <div id='app'>
-        <Header/>
-        <router-view/>
-        <Footer1/>
-        <Footer2/>
+    <div id="app">
+        <Header />
+        <router-view />
+        <Footer1 />
+        <Footer2 />
     </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import Header from '@/components/Header'
-import Footer1 from '@/components/Footer1'
-import Footer2 from '@/components/Footer2'
+import Header from "@/components/Header";
+import Footer1 from "@/components/Footer1";
+import Footer2 from "@/components/Footer2";
 export default {
     components: {
-        name: 'Home',
+        name: "Home",
         components: {
             Header,
             Footer1,
             Footer2
         }
     }
-}
+};
 </script>
 <style scoped>
 </style>
